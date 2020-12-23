48ad4cf5643030f12d5d52c2b1c117c3a26d29a2
radeax <joflip@gmail.com>
2020-12-23 18:30:07 -0500

    customer name is extracted from data.txt

--- main.sh
+++ main.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 echo Welcome to Pizza Planet!
+
+# Get name and insert into the variable, $name
 read -p "Please enter your name: " name
-sed -i "s/customerName=.*/customerName=$name/" data.txt
-echo "$name, pick your pizza"
+
+# Updates value of $customerName in data.txt to the value of $name here
+sed -i "s/customerName=.*/customerName=\"$name\"/" data.txt
+
+# executes the file. Variables in data.txt now exist here
+. data.txt 
+
+echo "$customerName, pick your pizza"
