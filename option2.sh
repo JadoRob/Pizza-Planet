ee68f397203b55d4a28ad7a909c3e32a91d0b29f
JadoRob <renojado@gmail.com>
2020-12-27 14:28:11 -0500

    Created the page for option 2 of main.sh to select size and type of crust

---
+++ option2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pizza="Meat Lover's Pizza"
+small=9.99; medium=11.99; large=14.99
+
+clear
+echo "$pizza"
+echo ""
+echo "Please choose by selecting a number:"
+echo ""
+echo "Size				Crust"
+echo "1. Small			4. Pan Crust"
+echo "2. Medium			5. Thin Crust"
+echo "3. Large			6. Stuffed Crust"
+echo "0. Back to main menu"
+echo ""
+echo "What size $pizza would you like?"
