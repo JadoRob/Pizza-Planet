66e63d87f80bc973b43e8845ff8d5695cc449a01
radeax <joflip@gmail.com>
2020-12-27 15:58:04 -0500

    Initial commit

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Main Menu
+
+#Display the current current page
+#Provide instructions to proceed
+#Display the options
+# 1. Pepperoni Pizza
+# 2. Meat Lover's Pizza
+# 3. Veggie Pizza
+
+#Display the greeting and ask for a name
+
+#Create conditionals regarding $customer name
+#Condition 1 - ask what they would like with no name
+#Condition 2 - ask what they would like with name provided
+
+#Create conditionals regarding if $cart is empty
+#Condition 1 - Ask what the customer would like if $cart is empty
+#Condition 2 - Ask if customer would like anything else if items exist in $cart
+#Condition 3 - Provide total, if customer cancels or changes order, start over
+#Condition 4 - If customer confirms, Display thank you with carry out/delivery msg
+#clear the cart
