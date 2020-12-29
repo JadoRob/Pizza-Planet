0676b9b05fff2a9bddb072a51335b4386782f5d7
radeax <joflip@gmail.com>
2020-12-28 20:42:38 -0500

    initial main menu draft

--- main.sh
+++ main.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 
-#Main Menu
+########### VARIABLES ###########
+choices=("Pepperoni Pizza" "Meat Lover's Pizza" "Veggie Pizza") # Menu items
+doneShopping=false  # Will be used below in a WHILE loop
 
-#Display the current current page
-#Provide instructions to proceed
-#Display the options
-# 1. Pepperoni Pizza
-# 2. Meat Lover's Pizza
-# 3. Veggie Pizza
+########### FUNCTIONS ###########
+displayOptions() {  # Displays menu items
+    for i in ${!choices[@]}; do
+        echo $(($i+1))\) ${choices[$i]}
+    done
+}
 
-#Display the greeting and ask for a name
+calcTotal() {   # Calculates total in cart.data
+    total=0
 
-#Create conditionals regarding $customer name
-#Condition 1 - ask what they would like with no name
-#Condition 2 - ask what they would like with name provided
+    for i in `cut -d ':' -f 4 cart.data`; do
+        i=`echo $i | sed "s/\"//g"`
+        total=$(bc <<< "$total + $i")
+    done
 
-#Create conditionals regarding if $cart is empty
-#Condition 1 - Ask what the customer would like if $cart is empty
-#Condition 2 - Ask if customer would like anything else if items exist in $cart
-#Condition 3 - Provide total, if customer cancels or changes order, start over
-#Condition 4 - If customer confirms, Display thank you with carry out/delivery msg
-#clear the cart
+    echo $total
+}
+
+########### START SCRIPT ###########
+read -p "Hello and welcome to Pizza Planet! May we have your name please? " customer
+
+printf "\nWhat can we get you today, $customer?\n"
+
+displayOptions
+
+while [ $doneShopping = false ]; do # Doesn't do anything yet. For now, just repeats until you answer with anything but Yy
+    read -p $'\nWould you like to add anything else (Y/N)? ' yn
+    if [ $yn == "Y" ] || [ $yn == "y" ]; then
+        printf "\nWhat would you like to add?\n"
+        displayOptions
+    else
+        doneShopping=true
+    fi
+done
+
+checkoutTotal=`calcTotal`   # calculate total
+
+printf "\nLooks great, your total comes to \$$checkoutTotal\n\n"
+
+read -p "Confirm purchase (Y/N)? " yn   # Yes will print goodbye message. For now, ends script regardless of answer
+
+if [ $yn == "Y" ] || [ $yn == "y" ]; then
+    printf "\nThank you for choosing Planet Pizza! You will be notified once your order is <read/on the way>.\n"
+    echo "Have a great day!"
+fi
