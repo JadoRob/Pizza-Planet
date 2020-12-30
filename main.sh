7530b2c65b53df66c3718ad7f6593bc28cf8ee22
radeax <joflip@gmail.com>
2020-12-30 11:30:57 -0500

    Link menu options with option1.sh & option2.sh

--- main.sh
+++ main.sh
@@ -6,9 +6,21 @@ doneShopping=false  # Will be used below in a WHILE loop
 
 ########### FUNCTIONS ###########
 displayOptions() {  # Displays menu items
+    echo --------------------------------------------
     for i in ${!choices[@]}; do
         echo $(($i+1))\) ${choices[$i]}
     done
+    echo --------------------------------------------
+    echo
+    selectOption
+}
+
+selectOption() {
+    read -p "[1-2] >> " option
+    case $option in
+        1) ./option1.sh ;;
+        2) ./option2.sh ;;
+    esac
 }
 
 calcTotal() {   # Calculates total in cart.data
@@ -22,14 +34,26 @@ calcTotal() {   # Calculates total in cart.data
     echo $total
 }
 
+##### incomplete - future feature
+#if grep -ixq "$customer" customers.txt; then
+#    echo 'customer exists'
+# This checks lines for exact name match
+#fi
+
 ########### START SCRIPT ###########
-read -p "Hello and welcome to Pizza Planet! May we have your name please? " customer
+
+> cart.data     # Clear cart
+
+echo Welcome to Pizza Planet!
+echo
+read -p "Please enter your name >> " customer
 
 printf "\nWhat can we get you today, $customer?\n"
 
 displayOptions
 
-while [ $doneShopping = false ]; do # Doesn't do anything yet. For now, just repeats until you answer with anything but Yy
+# Order additional items
+while [ $doneShopping = false ]; do
     read -p $'\nWould you like to add anything else (Y/N)? ' yn
     if [[ $yn =~ [Yy] ]]; then
         printf "\nWhat would you like to add?\n"
