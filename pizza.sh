ee96e63adea147d004b29cf2daa198d94eab1fba
Andrew Hopkins <you@example.com>
2021-01-02 15:54:38 -0500

    Modify for custome made pizza

--- pizza.sh
+++ pizza.sh
@@ -4,22 +4,26 @@ clear
 
 echo "Welcome to our pizza page"
 echo ""
-echo "Here are our selections for premade pizzas" 
+echo "Here are our selections for premade pizzas or custome make your own!!" 
 echo 1\) Pepperoni Pizza
 echo 2\) Meat lovers Pizza
 echo 3\) Veggie Pizza
 echo 4\) Cheese Pizza
-echo 5\) Main Menu
+echo 5\) Customize Your Own Pizza
+echo 6\) Main Menu
+
 echo ""
-sleep 2
-read -p "PLease make your selection from the premade pizzas above [1-4] or option 5 for main menu >> " choice
+
+echo "Please make your selection from the pizzas above [1-5]"
+read -p "or option 6 for main menu >> " choice
 
 case $choice in
 1) ./pepperoni.sh ;;
 2) ./option2.sh ;;
 3) ./Option3.sh ;;
 4) ./cheese.sh ;;
-5) ./menu2.sh ;;
+5) ./custome.sh;;
+6) ./menu2.sh ;;
 esac
 
 sleep 2
