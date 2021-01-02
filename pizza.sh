89ff6dca6972ec19a52d5453f319a792aa098419
Andrew Hopkins <you@example.com>
2021-01-02 15:58:26 -0500

    modification

--- pizza.sh
+++ pizza.sh
@@ -4,7 +4,7 @@ clear
 
 echo "Welcome to our pizza page"
 echo ""
-echo "Here are our selections for premade pizzas or custome make your own!!" 
+echo "Here are our selections for premade pizzas or custom make your own!!" 
 echo 1\) Pepperoni Pizza
 echo 2\) Meat lovers Pizza
 echo 3\) Veggie Pizza
@@ -22,7 +22,7 @@ case $choice in
 2) ./option2.sh ;;
 3) ./Option3.sh ;;
 4) ./cheese.sh ;;
-5) ./custome.sh;;
+5) ./custom.sh;;
 6) ./menu2.sh ;;
 esac
 
