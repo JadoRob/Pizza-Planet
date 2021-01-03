2ecc35c365cb0f6dee185617b5400539bb4131c6
Andrew Hopkins <you@example.com>
2021-01-03 15:16:05 -0500

    menu stuff

--- pizza.sh
+++ pizza.sh
@@ -1,29 +1,65 @@
 #!/bin/bash
+clear
+pizza=$1
+small=9.99; medium=11.99; large=14.99
 
+function showOptions {
 clear
 
-echo "Welcome to our pizza page"
+echo "Welcome to the $pizza pizza section of our menu!"
 echo ""
-echo "Here are our selections for premade pizzas or custom make your own!!" 
-echo 1\) Pepperoni Pizza
-echo 2\) Meat lovers Pizza
-echo 3\) Veggie Pizza
-echo 4\) Cheese Pizza
-echo 5\) Customize Your Own Pizza
-echo 6\) Main Menu
-
+echo "$pizza Pizza Menu Options"
 echo ""
-
-echo "Please make your selection from the pizzas above [1-5]"
-read -p "or option 6 for main menu >> " choice
-
-case $choice in
-1) ./pepperoni.sh ;;
-2) ./option2.sh ;;
-3) ./Option3.sh ;;
-4) ./cheese.sh ;;
-5) ./custom.sh;;
-6) ./menu2.sh ;;
-esac
-
-sleep 2
+echo "Size                      Crust"
+echo "1. Small                  4. Pan Crust"
+echo "2. Medium                 5. Thin Crust"
+echo "3. Large                  6. Stuffed Crust"
+echo "0. Back to Main Menu"
+echo ""
+}
+function getOrder {
+echo "What size $pizza pizza would you like? [1-3] >> "
+read option
+if (($option == 1))
+then
+size="Small"
+price=$small
+elif (($option == 2))
+then
+size="Medium"
+price=$medium
+elif (($option == 3))
+then
+size="Large"
+price=$large
+elif (($option == 0))
+then
+./menu.sh
+fi
+echo "What kind of crust would you like this $pizza pizza to have? [4-6] >> "
+read option
+if (($option == 4 ))
+then
+crust="Pan Crust"
+elif (($option == 5 ))
+then
+crust="Thin Crust"
+elif (($option == 6))
+then
+crust="Stuffed Crust"
+elif (($option == 0))
+then
+./menu.sh
+fi
+}
+until [ "$question" == "y" ]
+do
+showOptions
+getOrder
+echo "Add a $size $crust $pizza for $price to your order? [y/n] >> "
+read question
+done
+echo "$size:$crust:$pizza:$price" >> cart.data
+echo "Thank you, your order has been added. Returning to the main menu."
+sleep 3
+./menu.sh
