7c9ce1035336fad28538a0207a91e62fca6bb7b6
JadoRob <renojado@gmail.com>
2021-01-07 17:14:34 -0500

    Added data file for food, and header graphics

--- otherfood.sh
+++ otherfood.sh
@@ -18,13 +18,41 @@ choice="null"
 
 # function that displays options for the user
 # Displayed by reading from the file otherfood.data
+#function showMenu() {
+#	clear
+#	printf "============================================\n"
+#	printf "	                                   |\n"
+#	printf "	Welcome to PIZZA PLANET!!          |\n"
+#	printf "	                                   |\n"
+#	printf "============================================\n"
+#}
+
+function showGraphic() {
+	echo "                                          _.oo."
+	echo "                   _.u[[/;:,.         .odMMMMMM'"
+	echo "                 .o888UU[[[/;:-.  .o@P^    MMM^"
+	echo "                oN88888UU[[[/;::-.        dP^"
+	echo "               dNMMNN888UU[[[/;::-.   .o@P^"
+	echo "              ,MMMMMMN888UU[[/;::-. o@^"
+	echo "              NNMMMNN888UU[[[/~.o@P^"
+	echo "              888888888UU[[[/o@^-.."
+	echo "             oI8888uu[[[/o@P^:--.."
+	echo "          .@^  YUU[[[/o@^;::---.."
+	echo "        oMP     ^/o@P^;:::---.."
+	echo "     .dMMM    .o@^ ^;::---..."
+	echo "    dMMMMMMM@^ `      `^^^^"
+	echo "   YMMMUP^"
+	echo "    ^^"
+	figlet "Pizza Planet"
+	echo
+}
+
 function showOptions() {
-	clear
 	echo "Please choose by selecting a number:"
 	echo 
 	# read from otherfood.data, separate using ":" as the delimeter, print 
 	# column 1 of all lines along with line number
-	awk -F: '{print NR") "$1}' otherfood.data
+	awk -F: '{print NR") "$2}' otherfood.data
 	echo ""
 	echo "0. Back to main menu"
 	echo 
@@ -32,8 +60,9 @@ function showOptions() {
 
 function getOrder() {
 	read -p "What would you like?: " choice
-	product=$(awk -v var="$choice" -F: '{if(NR==var) print $1}' otherfood.data)
-	price=$(awk -v var="$choice" -F: '{if(NR==var) print $2}' otherfood.data)
+
+	product=$(awk -v var="$choice" -F: '{if(NR==var) print $2}' otherfood.data)
+	price=$(awk -v var="$choice" -F: '{if(NR==var) print $3}' otherfood.data)
 }
 
 #------------------------------------------------------------------------------------
@@ -41,6 +70,8 @@ function getOrder() {
 
 until [ "$confirm" == "y" ]
 do
+	clear
+	showGraphic | lolcat
 	showOptions
 	getOrder
 	read -p "Great choice, add $product for \$$price to your order? [y/n]: " confirm 
